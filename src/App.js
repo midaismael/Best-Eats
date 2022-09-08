f706c030391d32800ba34f61408f8369b4afdb42
Ahmed Ismael <midaismael@gmail.com>
2022-09-08 14:02:00 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "./Component/Hero";
 import Navbar from "./Component/Navbar";
 
 function App() {
   return (
     <div>
         <Navbar />
+        <Hero />
     </div>
   );
 }
